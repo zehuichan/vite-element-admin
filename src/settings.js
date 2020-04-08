bb9dcbc4504d8866c4eb5131c9bbf87e6cf553a6
jasconchan <jasonchenzehui@gmail.com>
2020-04-08 23:45:15 +0800

    Update settings.js

--- src/settings.js
+++ src/settings.js
@@ -24,11 +24,5 @@ module.exports = {
    * @type {boolean} true | false
    * @description Whether show the logo in sidebar
    */
-  sidebarLogo: false,
-
-  /**
-   * @type {boolean} true | false
-   * @description Whether show the logo in sidebar
-   */
   sidebarLogo: false
 }
