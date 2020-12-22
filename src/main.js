021b5e2e109eec3031cd23f3f6e9fccae04c3a88
jasonchenzehui@gmail.com <jasonchenzehui@gmail.com>
2020-12-22 20:06:31 +0800

    d

--- src/main.js
+++ src/main.js
@@ -5,9 +5,10 @@ import Cookies from 'js-cookie'
 import 'normalize.css/normalize.css' // A modern alternative to CSS resets
 
 import Element from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
+import '@/styles/scss/element-variables.scss'
 
 import VComponents from '@/constants/vcomponents' // global components
+
 import '@/styles/scss/index.scss' // global css
 
 import App from './App'
