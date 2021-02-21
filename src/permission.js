bd135fd85935e31e9540567213362f2de6486e34
jasconchan <jasonchenzehui@gmail.com>
2021-02-21 22:23:53 +0800

    d

--- src/permission.js
+++ src/permission.js
@@ -34,7 +34,7 @@ router.beforeEach(async (to, from, next) => {
         try {
           // get user info
           // note: roles must be a object array! such as: ['admin'] or ,['developer','editor']
-          const { roles } = await store.dispatch('user/getInfo')
+          const {roles} = await store.dispatch('user/getInfo')
 
           // generate accessible routes map based on roles
           const accessRoutes = await store.dispatch('permission/generateRoutes', roles)
@@ -44,7 +44,7 @@ router.beforeEach(async (to, from, next) => {
 
           // hack method to ensure that addRoutes is complete
           // set the replace: true, so the navigation will not leave a history record
-          next({ ...to, replace: true })
+          next({...to, replace: true})
         } catch (error) {
           // remove token and go to login page to re-login
           await store.dispatch('user/resetToken')
