252bcac3db522691d6cdf25323dc565f9da35c2f
chenzehui <jasonchenzehui@gmail.com>
2021-09-30 13:55:59 +0800

    d

---
+++ config/index.js
@@ -0,0 +1,1 @@
+console.log(process.env.VUE_APP_ENV)
--- src/views/vcomponents/vexcel.vue
+++ src/views/vcomponents/vexcel.vue
@@ -4,10 +4,17 @@
     <demo-section>
       <demo-card title="上传下载Excel">
         <demo-block title="v-excel-export前端导出excel">
-          <v-excel-export type="warning" icon="el-icon-upload2">前端导出excel</v-excel-export>
+          <v-excel-export
+            type="warning"
+            icon="el-icon-download"
+            :filename="filename"
+            :can-export="canExport"
+            :t-header="tHeader"
+            :t-body="tBody">前端导出excel
+          </v-excel-export>
         </demo-block>
         <demo-block title="v-excel-upload前端上传excel数据转json">
-          <v-excel-upload type="success" icon="el-icon-download" @success="onSuccess">上传Excel</v-excel-upload>
+          <v-excel-upload type="success" icon="el-icon-upload2" @success="onSuccess">上传Excel</v-excel-upload>
         </demo-block>
         <demo-block title="v-uploader前端原生上传">
           <v-uploader type="info" icon="el-icon-upload">原生上传</v-uploader>
@@ -26,40 +33,57 @@
 </template>
 
 <script>
-  // mixins
-  import comp from './comp'
-  // code
-  import {vexcel} from './code'
+// mixins
+import comp from './comp'
+// code
+import { vexcel } from './code'
 
-  export default {
-    name: 'vexcel',
-    mixins: [comp],
-    data() {
-      return {
-        vexcel,
+export default {
+  name: 'vexcel',
+  mixins: [comp],
+  data() {
+    return {
+      vexcel,
+      tableData: [
+        { name: 'John Brown', age: '32', address: 'New York No. 1 Lake Park', tags: ['NICE', 'DEVELOPER'] },
+        { name: 'Jim Green', age: '42', address: 'London No. 1 Lake Park', tags: ['LOSER'] },
+        { name: 'Joe Black', age: '32', address: 'Sidney No. 1 Lake Park', tags: ['COOL', 'TEACHER'] },
+      ],
 
-        upload_data: []
-      }
-    },
-    methods: {
-      onSuccess({results, header}) {
-        for (let i = 0; i < results.length; i++) {
-          let each = results[i]
-          each = this.transExcelRow(each)
-          this.upload_data.push({
-            deliveryNumber: each['快递单号'],
-            expressCompanyId: 1,
-            orderId: each['订单号']
-          })
-        }
-      },
-      transExcelRow(row) {
-        const ret = {}
-        for (const i in row) {
-          ret[i.trim()] = row[i]
-        }
-        return ret
-      },
+      filename: 'vtable',
+      canExport: true,
+      tHeader: ['Name', 'Age', 'Address', 'Tags'],
+      tFilters: ['name', 'age', 'address', 'tags'],
+      upload_data: []
     }
+  },
+  computed: {
+    tBody() {
+      return this.tableData.map(v => this.tFilters.map(j => {
+        return v[j]
+      }))
+    }
+  },
+  methods: {
+    onSuccess({ results, header }) {
+      for (let i = 0; i < results.length; i++) {
+        let each = results[i]
+        each = this.transExcelRow(each)
+        this.upload_data.push({
+          name: each['Name'],
+          age: each['Age'],
+          address: each['Address'],
+        })
+      }
+      console.log(this.upload_data)
+    },
+    transExcelRow(row) {
+      const ret = {}
+      for (const i in row) {
+        ret[i.trim()] = row[i]
+      }
+      return ret
+    },
   }
+}
 </script>
