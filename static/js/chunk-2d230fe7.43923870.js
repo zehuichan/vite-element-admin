abce957335009abe7cc7c8381d85b0d2dabc7f7a
zehuichan <zehuichan@users.noreply.github.com>
2023-01-28 10:01:46 +0000

    deploy: 855653e26e5b5fd5a54f9661e132247177078cda

---
+++ static/js/chunk-2d230fe7.43923870.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d230fe7"],{ef3c:function(e,r,t){"use strict";t.r(r);var n,u,a={created(){const{params:e,query:r}=this.$route,{path:t}=e;this.$router.replace({path:"/"+t,query:r})},render:function(e){return e()}},c=a,o=t("2877"),p=Object(o["a"])(c,n,u,!1,null,null,null);r["default"]=p.exports}}]);
